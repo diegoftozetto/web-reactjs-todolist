357f40c68b586bac22ec370e3af76959f2126574
Diego Tozetto <diegotozetto@gmail.com>
2021-07-29 11:13:08 -0300

    Cria componente button

---
+++ src/components/Button/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  background-color: rgb(46, 95, 148);
+  cursor: pointer;
+  color: white;
+
+  height: 35px;
+  width: 100%;
+  margin: 10px 0;
+`;
