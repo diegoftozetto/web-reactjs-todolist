7a3f6473d6016af69e1c8e5a760c12a6851e26f3
Diego Tozetto <diegotozetto@gmail.com>
2021-07-30 00:51:37 -0300

    Atualiza o template Login

--- src/templates/Login/index.jsx
+++ src/templates/Login/index.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { useEffect } from 'react';
 import { FormLogin } from '../../components/FormLogin';
 import { selectors } from '../../selectors/user';
 
@@ -7,9 +8,11 @@ export const Login = () => {
   const isLogged = useSelector(selectors.getIsLogged);
   const history = useHistory();
 
-  if (isLogged) {
-    history.push('/task');
-  }
+  useEffect(() => {
+    if (isLogged) {
+      history.push('/task');
+    }
+  }, [history, isLogged]);
 
   return <FormLogin />;
 };
